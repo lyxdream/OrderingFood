ff66c8e7b48e4e3b38d8b1f98ff7179a19acb111
liyinxia <liyinxia@rtmap.com>
2021-01-15 11:36:29 +0800

    购物车逻辑修改

---
+++ test.js
@@ -0,0 +1,16 @@
+
+let params = [1,2,3]
+
+
+let arr = params.forEach((item) => {
+      item = item * 2
+      console.log(item)
+})
+
+
+let arr1 = params.map((item) => {
+   return item = item * 2
+})
+console.log(arr)
+console.log(arr1)
+console.log(params)
