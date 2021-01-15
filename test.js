fc47322d5d3a4bec3d0b017f99b897606e4278e9
liyinxia <liyinxia@rtmap.com>
2021-01-15 16:31:55 +0800

    购物车逻辑补充

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
-
-let params = [1,2,3]
-
+let params = [1, 2, 3]
 
 let arr = params.forEach((item) => {
-      item = item * 2
-      console.log(item)
+    item = item * 2
+    console.log(item)
 })
 
-
 let arr1 = params.map((item) => {
-   return item = item * 2
+    return (item = item * 2)
 })
 console.log(arr)
 console.log(arr1)
 console.log(params)
+
+
